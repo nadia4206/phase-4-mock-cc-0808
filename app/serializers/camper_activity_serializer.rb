ca531fd921bf131f2e12b4d02080d223a734cf58
Nadia Virjee <nadvirjee@gmail.com>
2023-03-01 11:05:35 -0500

    commit tree a22aa87867c27a454ce347cae26722b385dbaad2

---
+++ app/serializers/camper_activity_serializer.rb
@@ -0,0 +1,16 @@
+class CamperActivitySerializer < ActiveModel::Serializer
+  attributes :id, :name, :difficulty
+
+  def id
+    self.object.activity.id
+  end
+
+  def name
+    self.object.activity.name
+  end
+
+  def difficulty
+    self.object.activity.difficulty
+  end
+
+end
